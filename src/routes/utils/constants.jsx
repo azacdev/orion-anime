ad2273510761286b77b97b61d4bf66246e458aa1
azacdev <azacdev@gmail.com>
2023-04-26 04:22:14 +0100

    update

--- src/routes/utils/constants.jsx
+++ src/routes/utils/constants.jsx
@@ -1,22 +1,28 @@
+import { GiMaterialsScience, GiPunch, GiPuzzle, GiAlienStare, GiMagicPortal, GiFullPizza, GiFullPizza, GiLovers, GiDramaMasks  } from "react-icons/gi"
+import { BsArrowThroughHeartFill, BsAward } from "react-icons/bs"
+import { HiSparkles } from "react-icons/hi"
+import { TbPumpkinScary } from "react-icons/tb"
+import { MdSportsSoccer, MdOutlineTheaterComedy } from "react-icons/md"
+import { BiMovie } from "react-icons/bi"
 
 
 export const categories = [
-  { name: 'Action', icon: <CodeIcon />, },
-  { name: 'Award Winning', icon: <HomeIcon />, },
-  { name: 'Horror', icon: <CodeIcon />, },
-  { name: 'Ecchi', icon: <CodeIcon />, },
-  { name: 'Sports', icon: <SchoolIcon />, },
-  { name: 'Supernatural', icon: <GraphicEqIcon />, },
-  { name: 'Fantasy', icon: <OndemandVideoIcon />, },
-  { name: 'Gourmet', icon: <SportsEsportsIcon />, },
-  { name: 'Boys Love', icon: <LiveTvIcon />, },
-  { name: 'Drama', icon: <FitnessCenterIcon />, },
-  { name: 'Comedy', icon: <CheckroomIcon />, },
-  { name: 'Mystery', icon: <FaceRetouchingNaturalIcon />, },
-  { name: 'Girls Love', icon: <TheaterComedyIcon />, },
-  { name: 'Slice of Life', icon: <FitnessCenterIcon />, },
-  { name: 'Adventure', icon: <DeveloperModeIcon />, },    
-  { name: 'Romance', icon: <DeveloperModeIcon />, },    
-  { name: 'Sci-Fi', icon: <DeveloperModeIcon />, },    
-  { name: 'Hentai', icon: <DeveloperModeIcon />, }    
+  { name: 'Action', icon: <GiPunch /> },
+  { name: 'Award Winning', icon: <BsAward />, },
+  { name: 'Horror', icon: <TbPumpkinScary />, },
+  { name: 'Ecchi', icon: <HiSparkles />, },
+  { name: 'Sports', icon: <MdSportsSoccer  />, },
+  { name: 'Supernatural', icon: <GiAlienStare />, },
+  { name: 'Fantasy', icon: <GiMagicPortal />, },
+  { name: 'Gourmet', icon: <GiFullPizza />, },
+  { name: 'Boys Love', icon: <GiLovers/>, },
+  { name: 'Drama', icon: <GiDramaMasks />, },
+  { name: 'Comedy', icon: <MdOutlineTheaterComedy />, },
+  { name: 'Mystery', icon: <GiPuzzle />, },
+  { name: 'Girls Love', icon: <GiLovers />, },
+  { name: 'Slice of Life', icon: <BiMovie />, },
+  { name: 'Adventure', icon: <FaMountain />, },    
+  { name: 'Romance', icon: <BsArrowThroughHeartFill />, },    
+  { name: 'Sci-Fi', icon: <GiMaterialsScience />, },    
+  { name: 'Hentai', icon: <HiSparkles />, }    
 ]
