3c459570ce7608167009faf6df2cc5691c6b9ca2
azacdev <azacdev@gmail.com>
2023-04-29 22:07:28 +0100

    update

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
+import { createContext } from 'react';
 import SearchIcon from '../search.svg' ;
 import { IoIosContact } from "react-icons/io"
 import { CgDarkMode } from 'react-icons/cg'
@@ -6,7 +7,11 @@ import Sidebar from "./Sidebar"
 import { fetchFromAPI } from './utils/fetchFromAPI';
 import AnimeCard from './AnimeCard'
 
+export const ThemeContext = createContext(null)
+
+
 const Root = () => {
+  const [theme, setTheme] = useState(null)
   const [isDarkMode, setisDarkMode] = useState(false)
   const [selectedGenre, setSelectedGenre ] = useState("")
   const [animeList, setAnimeList] = useState([])
@@ -24,45 +29,47 @@ const Root = () => {
   // console.log(titles);
 
   return (
-    <div className="container" id="dark">
-      <header className='scroll-bar'>
-        <Sidebar />
-      </header>
+    <ThemeContext.Provider value={theme, setTheme}>
+      <div className="container" id="dark">
+        <header className='scroll-bar'>
+          <Sidebar />
+        </header>
 
-      <main className="section scroll-bar">
-        <nav>
-          <a href='#' className='nav-icons'>
-            <CgDarkMode/>
-          </a>
-
-          <div className="search">
-            <input
-              // value={searchTerm}
-              // onChange={(e) => setSeacrhTerm(e.target.value)}
-            />
-            <img
-              src={SearchIcon}
-              alt="search"
-              // onClick={() => searchMovies(searchTerm)}
-            />
-
-          </div>
-
-          <div className='login'>
-            <p>LOGIN</p>
+        <main className="section scroll-bar">
+          <nav>
             <a href='#' className='nav-icons'>
-              <IoIosContact/>
+              <CgDarkMode/>
             </a>
-          </div>
-        </nav>
 
-        <div className="content">
-          {animeList.map((item, idx) =>
-            (item && <AnimeCard video={item} key={idx}/>)
-          )}
-        </div>
-      </main>
-    </div>
+            <div className="search">
+              <input
+                // value={searchTerm}
+                // onChange={(e) => setSeacrhTerm(e.target.value)}
+              />
+              <img
+                src={SearchIcon}
+                alt="search"
+                // onClick={() => searchMovies(searchTerm)}
+              />
+
+            </div>
+
+            <div className='login'>
+              <p>LOGIN</p>
+              <a href='#' className='nav-icons'>
+                <IoIosContact/>
+              </a>
+            </div>
+          </nav>
+
+          <div className="content">
+            {animeList.map((item, idx) =>
+              (item && <AnimeCard video={item} key={idx}/>)
+            )}
+          </div>
+        </main>
+      </div>
+    </ThemeContext.Provider>
   )
 }
 
