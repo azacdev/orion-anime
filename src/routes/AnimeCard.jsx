26cfcad5c37b2fb460de1f5d705e846b9d3b320f
azacdev <azacdev@gmail.com>
2023-05-06 23:25:04 +0100

    update

--- src/routes/AnimeCard.jsx
+++ src/routes/AnimeCard.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom"
 
-const AnimeCard = ({video}) => {
+const AnimeCard = ({video: {_id}}) => {
+  
   return (
     <div className="anime-content">
-      <Link to="/animes/:id" className="anime-card">
+      <Link to={`/animes/${_id}`} className="anime-card">
         <img src={video.image} className="anime-image" alt="anime-image" />
         <p className="anime-title">{video.title}</p>
       </Link>
