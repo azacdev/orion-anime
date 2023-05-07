0c361370a48389605d6a1dd470d1afa72cda2302
azacdev <azacdev@gmail.com>
2023-05-07 23:30:03 +0100

    update

--- src/routes/AnimeDetails.jsx
+++ src/routes/AnimeDetails.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { fetchFromAPI } from './utils/fetchFromAPI'
 
 const AnimeDetails = () => {
   const { id } = useParams()
   const [animeDetails, setAnimeDetails] = useState()
   
+
   return (
     <div className='anime-details'>AnimeDetails</div>
   )
