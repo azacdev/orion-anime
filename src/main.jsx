6db36f31eb2e4616b08766b9ae6e8096f63fdc9d
azacdev <azacdev@gmail.com>
2023-05-12 23:47:02 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import  Root  from './routes/root'
 import { AnimeDetails } from './routes'
 import ErrorPage from './error-page'
+import { store } from '@reduxjs/toolkit'
 import './App.css'
 
 const router = createBrowserRouter([
