3192f1f5393975c121014fd153763d6167710930
azacdev <azacdev@gmail.com>
2023-05-12 22:53:43 +0100

    update

--- src/routes/Main.jsx
+++ src/routes/Main.jsx
@@ -3,48 +3,50 @@ import { AnimeCard } from "./"
 import { useState } from 'react';
 
 const Main = ({ animeList, searchResult }) => {
-  const [currentPage, setCurrentPage] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1)
+  const [animesPerPage] = useState(24)
 
+  // Calculate the index of the first and last movies to be displayed on the current page
+  const indexOfLastAnime = currentPage * animesPerPage; // 24
+  const indexOfFirstAnime = indexOfLastAnime - animesPerPage; // 0
+
+  // Get the movies to be displayed on the current page
+  const currentAnimes = animeList.slice(indexOfFirstAnime, indexOfLastAnime)
+
+    // Change the current page
   const handlePageChange = (pageNumber) => {
-    setCurrentPage(pageNumber);
-  };
+    setCurrentPage(pageNumber)
+  }
+
+   // Calculate the total number of pages based on the number of movies and the movies per page
+   const totalPages = Math.ceil(movies.length / moviesPerPage);
+
+   // Generate an array of page numbers for the pagination buttons
+   const pageNumbers = [];
+   for (let i = 1; i <= totalPages; i++) {
+     pageNumbers.push(i);
+   }
   
   return (
     <div>
       {searchResult.length > 0 ?
         (
           <div className="content">
-            {searchResult.slice(currentPage * 20, (currentPage + 1) * 20).map((item, idx) => (
+            {searchResult.slice(currentPage * 24, (currentPage + 1) * 24).map((item, idx) => (
               <div key={idx} className="anime-content">
                 {item && <AnimeCard video={item} key={idx}/>}
               </div>
             ))}
-            <ReactPaginate
-              previousLabel="Previous"
-              nextLabel="Next"
-              currentPage={currentPage}
-              onPageChange={handlePageChange}
-              totalPages={Math.ceil(movies.length / 20)}
-              pageSize={20}
-          />
           </div>
         ) 
         :
         (
           <div className="content">
-            {animeList.slice(currentPage * 20, (currentPage + 1) * 20).map((item, idx) => (
+            {animeList.slice(currentPage * 24, (currentPage + 1) * 24).map((item, idx) => (
               <div key={idx} className="anime-content">
                 {item && <AnimeCard video={item} key={idx}/>}
               </div>
             ))}
-            <ReactPaginate
-              previousLabel="Previous"
-              nextLabel="Next"
-              currentPage={currentPage}
-              onPageChange={handlePageChange}
-              totalPages={Math.ceil(movies.length / 20)}
-              pageSize={20}
-      />
           </div>
         )
       }
