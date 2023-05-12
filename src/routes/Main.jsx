3d70faa1a45a2b769d77e20bfbf98623b7cacdf1
azacdev <azacdev@gmail.com>
2023-05-12 22:31:10 +0100

    update

--- src/routes/Main.jsx
+++ src/routes/Main.jsx
@@ -1,16 +1,32 @@
+import ReactPaginate from 'react-paginate';
 import { AnimeCard } from "./"
+import { useState } from 'react';
 
 const Main = ({ animeList, searchResult }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+  
   return (
     <div>
       {searchResult.length > 0 ?
         (
           <div className="content">
-            {searchResult.map((item, idx) => (
+            {searchResult.slice(currentPage * 20, (currentPage + 1) * 20).map((item, idx) => (
               <div key={idx} className="anime-content">
                 {item && <AnimeCard video={item} key={idx}/>}
               </div>
             ))}
+            <ReactPaginate
+              previousLabel="Previous"
+              nextLabel="Next"
+              currentPage={currentPage}
+              onPageChange={handlePageChange}
+              totalPages={Math.ceil(movies.length / 20)}
+              pageSize={20}
+          />
           </div>
         ) 
         :
@@ -21,6 +37,14 @@ const Main = ({ animeList, searchResult }) => {
                 {item && <AnimeCard video={item} key={idx}/>}
               </div>
             ))}
+            <ReactPaginate
+              previousLabel="Previous"
+              nextLabel="Next"
+              currentPage={currentPage}
+              onPageChange={handlePageChange}
+              totalPages={Math.ceil(movies.length / 20)}
+              pageSize={20}
+      />
           </div>
         )
       }
