322f1a3a198a311d1c290b9b35b44680658f5768
azacdev <azacdev@gmail.com>
2023-07-22 00:10:39 +0100

    anime details banner

--- src/pages/AnimeDetails.jsx
+++ src/pages/AnimeDetails.jsx
@@ -25,10 +25,25 @@ const AnimeDetails = () => {
           className="w-screen h-52 object-cover blur-lg opacity-60 z-0"
         />
       </div>
-      <div className="containerWrap px-4">
+
+      <div className="containerWrap relative px-4 z-10">
         <Link to="/" className="flex items-start text-2xl py-2 sm:py-6">
           <FaArrowLeft />
         </Link>
+        <div className="flex flex-col gap-8">
+          <div className="grid grid-cols-3 gap-5">
+            <div className="flex flex-col gap-5">
+              <img
+                src={images?.jpg.large_image_url}
+                className="object-cover rounded-xl w-52"
+                loading="lazy"
+                alt="anime-image"
+              />
+            </div>
+
+            <div className="flex flex-col gap-4 justify-start col-span-2 mt-16"></div>
+          </div>
+        </div>
       </div>
     </div>
   );
