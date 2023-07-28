dba035e1eeaaaca341e151a0efafbfdff3bb3016
azacdev <azacdev@gmail.com>
2023-07-28 23:42:38 +0100

    fixed: anime details loading state

--- src/pages/AnimeDetails.jsx
+++ src/pages/AnimeDetails.jsx
@@ -21,7 +21,7 @@ const AnimeDetails = () => {
 
   return (
     <div className="pb-24">
-      {isLoading && (
+      {isLoading ? (
         <div className="h-screen grid place-content-center">
           <svg
             version="1.1"
@@ -66,39 +66,42 @@ const AnimeDetails = () => {
             </circle>
           </svg>
         </div>
-      )}
-      <div className="containerWrap absolute top-0 left-0 right-0 w-full">
-        <img
-          src={images?.jpg.small_image_url}
-          className="w-full h-24 sm:h-36 lg:h-52 object-cover blur-lg opacity-60 z-0"
-        />
-      </div>
-
-      <div className="containerWrap relative px-4 z-10">
-        <div className="flex flex-col gap-8 mt-4 sm:mt-14 lg:mt-20">
-          <AnimeDescriptionDetails animeData={animeDetails} />
-
-          <div className="grid-cols-1 lg:grid-cols-2 grid gap-5">
-            {trailer?.embed_url && (
-              <div className="flex flex-col gap-3">
-                <h1 className="text-base sm:text-xl font-black uppercase">
-                  Trailer
-                </h1>
-
-                <ReactPlayer
-                  className="react-player relative z-2 rounded-md"
-                  url={trailer?.embed_url}
-                  controls
-                  width="100%"
-                  height="100%"
-                />
-              </div>
-            )}
-            <AnimeCharacterDetails trailer={trailer} id={id} />
+      ) : (
+        <>
+          <div className="containerWrap absolute top-0 left-0 right-0 w-full">
+            <img
+              src={images?.jpg.small_image_url}
+              className="w-full h-24 sm:h-36 lg:h-52 object-cover blur-lg opacity-60 z-0"
+            />
           </div>
-          <RelatedAnimeDetails id={id} />
-        </div>
-      </div>
+
+          <div className="containerWrap relative px-4 z-10">
+            <div className="flex flex-col gap-8 mt-4 sm:mt-14 lg:mt-20">
+              <AnimeDescriptionDetails animeData={animeDetails} />
+
+              <div className="grid-cols-1 lg:grid-cols-2 grid gap-5">
+                {trailer?.embed_url && (
+                  <div className="flex flex-col gap-3">
+                    <h1 className="text-base sm:text-xl font-black uppercase">
+                      Trailer
+                    </h1>
+
+                    <ReactPlayer
+                      className="react-player relative z-2 rounded-md"
+                      url={trailer?.embed_url}
+                      controls
+                      width="100%"
+                      height="100%"
+                    />
+                  </div>
+                )}
+                <AnimeCharacterDetails trailer={trailer} id={id} />
+              </div>
+              <RelatedAnimeDetails id={id} />
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
